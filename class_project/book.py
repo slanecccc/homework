988d55625011d97491c92bc4451afde25b772d38
Goryachev k.s <yandex.kost@gmail.com>
2023-10-20 00:15:26 +0300

    Создал класс Auter  и добавил его в класс Book

--- class_project/book.py
+++ class_project/book.py
@@ -1,6 +1,18 @@
+class Author:
+    def __init__(self, full_name: str, year_birth: str, birthplace: str):
+        self. full_name = full_name
+        self.year_birth = year_birth
+        self.birthplace = birthplace
+
+    def __str__(self):
+        return f"{self.full_name} \n" \
+               f" - Дата рождения: {self.year_birth} \n" \
+               f" - Место рождения: {self.birthplace} "
+
+
 class Book:
     def __init__(self, title_book: str, year_release: int, publisher: str,
-                 genre: str, author: str, price: int):
+                 genre: str, author: Author, price: int):
         self.title = title_book
         self.release = year_release
         self.publisher = publisher
