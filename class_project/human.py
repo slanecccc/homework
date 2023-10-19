c25226b89316d6facccc5c4f9719f9116a6f04f1
Goryachev k.s <yandex.kost@gmail.com>
2023-10-20 00:04:05 +0300

    Корректировка модуля human.py

--- class_project/human.py
+++ class_project/human.py
@@ -9,29 +9,26 @@ class FullName:
 
 
 class HomeAddress:
-    def __init__(self, street: str, house_number: int, flat: int):
+    def __init__(self, country: str, city: str, street: str, house_number: int, flat: int):
+        self.country = country
+        self.city = city
         self.street = street
         self.house = house_number
         self.flat = flat
 
     def __str__(self) -> str:
-        return f"ул.{self.street} д.{self.house} кв.{self.flat}"
+        return f"{self.country}, г. {self.city}, ул.{self.street}, д.{self.house}, кв.{self.flat}"
 
 
 class Person:
-    def __init__(self, full_name: FullName, date_birth: str, phone: str, country: str,
-                 city: str, home_address: HomeAddress):
+    def __init__(self, full_name: FullName, date_birth: str, phone: str, home_address: HomeAddress):
         self.name = full_name
         self.birth = date_birth
         self.phone = phone
-        self.country = country
-        self.city = city
         self.address = home_address
 
     def __str__(self):
         return f"ФИО: {self.name} \n" \
                f"Дата рождения: {self.birth} \n" \
                f"Контактный телелефон: {self.phone} \n" \
-               f"Страна: {self.country} \n" \
-               f"Город: {self.city} \n" \
                f"Домашний адресс: {self.address}"
--- main.py
+++ main.py
@@ -5,9 +5,8 @@ from class_project.book import *
 def main():
 
     full_name = FullName("Горячев", "Константин", "Сергеевич")
-    home_address = HomeAddress("Ньютона", 49, 5)
-    person = Person(full_name, "30.12.1999", "+7(905)556-43-43", "Россия",
-                    "Ярославль", home_address)
+    home_address = HomeAddress("Россия", "Ярославль", "Ньютона", 49, 5)
+    person = Person(full_name, "30.12.1999", "+7(905)556-43-43", home_address)
     print(person)
 
     book = Book("Колобок", 1999, "Ромашка", "Ужасы", "Толстой А.Н.", 99999)
