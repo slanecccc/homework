609a05ebd59ec5e594f0858f93879192dc598603
Goryachev k.s <yandex.kost@gmail.com>
2023-10-23 01:42:07 +0300

    Реализация обработки исключений в файле main.py

--- main.py
+++ main.py
@@ -3,15 +3,17 @@ from class_project.book import *
 
 
 def main():
-
-    full_name = FullName("Горячев", "Константин", "Сергеевич")
-    home_address = HomeAddress("Россия", "Ярославль", "Ньютона", 49, 5)
-    person = Person(full_name, "30.12.1999", "+7(905)556-43-43", home_address)
-    print(person)
-
-    autor = Author("Толстой А.Н.", "31.11.1812", "Россия")
-    book = Book("Колобок", 1999, "Ромашка", "Ужасы", autor, 99999)
-    print(book)
+    try:
+        full_name = FullName("Горячев", "Константин", "Сергеевич")
+        home_address = HomeAddress("Россия", "Ярославль", "Ньютона", 49, 5)
+        person = Person(full_name, "30.12.1999", "+7(905)556-43-43", home_address)
+        autor = Author("Толстой А.Н.", "31.11.1812", "Россия")
+        book = Book("Колобок", 1999, "Ромашка", "Ужасы", autor, 99999)
+    except ValueError as e:
+        print(e)
+    else:
+        print(person)
+        print(book)
 
 
 if __name__ == '__main__':
