b5802e8f0e68e14c13c14de30213a9c114aed0e5
Goryachev K.S. <google.kost@yandex.ru>
2024-03-06 23:39:49 +0300

    Реализовал методы area и perimeter в классе Ellipde и реализовал init

--- shape_module.py
+++ shape_module.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from math import pi
+from math import pi, sqrt
 class Shape(ABC):
     @abstractmethod
     def area(self):
@@ -8,10 +8,10 @@ class Shape(ABC):
     def perimeter(self):
         ...
     @abstractmethod
-    def save(self):
+    def save(self, path: str):
         ...
     @abstractmethod
-    def load(self):
+    def load(self, path: str):
         ...
 
 
@@ -40,6 +40,11 @@ class Rectangle(Shape):
     def perimeter(self):
         return (self.__width * self.__height) * 2
 
+    def save(self, path: str):
+        with open(path, 'a', encoding= 'utf-8') as file:
+            ...
+
+
 class Circle(Shape):
     def __init__(self, coordinate: tuple, radius: int):
         self.__coordinate: tuple = coordinate
@@ -52,4 +57,13 @@ class Circle(Shape):
         return self.__radius * pi * 2
 
 class Ellipse(Shape):
-    ...
+    def __init__(self, coordinate: tuple, width_el: int, height_el: int ):
+        self.__coordinate: tuple = coordinate
+        self.__width_el: int = width_el
+        self.__height_el: int = height_el
+
+    def area(self):
+        return pi * (self.__height_el/ 2) * (self.__width_el / 2)
+
+    def perimeter(self):
+        return 2 * pi *sqrt((self.__width_el ** 2 + self.__height_el ** 2) / 8)
