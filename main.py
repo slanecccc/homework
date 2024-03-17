7c5691110994ffe3cd5516acc543aa553648c78b
Goryachev K.S. <google.kost@yandex.ru>
2024-03-18 00:43:32 +0300

    Создал объекты классов фигур в файле main

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from shape_module import *
 def main():
-    ...
+    square = Square((2, 3), 4)
+    rectangle = Rectangle((4, 5), 5, 10)
+    circle = Circle((1, 6), 6)
+    ellipse = Ellipse((6, 7), 4, 10)
+    list_figure = [square, rectangle, circle, ellipse]
 
 
 if __name__ == '__main__':
