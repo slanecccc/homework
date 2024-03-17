5aaf85a6c4f02e21e492d85da9ec9fea702e105c
Goryachev K.S. <google.kost@yandex.ru>
2024-03-18 00:46:44 +0300

    Реализовал метод save в классах фигур

--- shape_module.py
+++ shape_module.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 from math import pi, sqrt
+
+
 class Shape(ABC):
     @abstractmethod
     def area(self):
@@ -8,10 +10,10 @@ class Shape(ABC):
     def perimeter(self):
         ...
     @abstractmethod
-    def save(self, path: str):
+    def save(self):
         ...
     @abstractmethod
-    def load(self, path: str):
+    def load(self):
         ...
 
 
@@ -21,11 +23,19 @@ class Square(Shape):
         self.__side: int = side
 
     def area(self):
-        return self.__side * 2
+        return round(self.__side * 2, 2)
 
     def perimeter(self):
-        return self.__side * 4
+        return round(self.__side * 4, 2)
 
+    def save(self):
+        __name_file = "Squares.txt"
+        with open(__name_file, 'a', encoding='utf-8') as file:
+            file.writelines(["Square,", str(self.__side) + ',', str(self.__coordinate) + ',',
+                             str(self.perimeter()) + ',', str(self.area())])
+
+    def load(self):
+        ...
 
 
 class Rectangle(Shape):
@@ -35,14 +45,19 @@ class Rectangle(Shape):
         self.__height: int = height
 
     def area(self):
-        return self.__width * self.__height
+        return round(self.__width * self.__height, 2)
 
     def perimeter(self):
-        return (self.__width * self.__height) * 2
+        return round((self.__width * self.__height) * 2, 2)
 
-    def save(self, path: str):
-        with open(path, 'a', encoding= 'utf-8') as file:
-            ...
+    def save(self):
+        __name_file = "Rectangles.txt"
+        with open(__name_file, 'a', encoding='utf-8') as file:
+            file.writelines(["Rectangle,", str(self.__width) + ',', str(self.__height) + ',',
+                             str(self.__coordinate) + ',', str(self.perimeter()) + ',', str(self.area())])
+
+    def load(self):
+        ...
 
 
 class Circle(Shape):
@@ -51,10 +66,19 @@ class Circle(Shape):
         self.__radius: int = radius
 
     def area(self):
-        return (self.__radius * pi) ** 2
+        return round((self.__radius * pi) ** 2, 2)
 
     def perimeter(self):
-        return self.__radius * pi * 2
+        return round(self.__radius * pi * 2, 2)
+
+    def save(self):
+        __name_file = "Circles.txt"
+        with open(__name_file, 'a', encoding='utf-8') as file:
+            file.writelines(["Circle,", str(self.__radius) + ',',
+                             str(self.__coordinate) + ',', str(self.perimeter()) + ',', str(self.area())])
+
+    def load(self):
+        ...
 
 class Ellipse(Shape):
     def __init__(self, coordinate: tuple, width_el: int, height_el: int ):
@@ -63,7 +87,16 @@ class Ellipse(Shape):
         self.__height_el: int = height_el
 
     def area(self):
-        return pi * (self.__height_el/ 2) * (self.__width_el / 2)
+        return round(pi * (self.__height_el/ 2) * (self.__width_el / 2), 2)
 
     def perimeter(self):
-        return 2 * pi *sqrt((self.__width_el ** 2 + self.__height_el ** 2) / 8)
+        return round(2 * pi * sqrt((self.__width_el ** 2 + self.__height_el ** 2) / 8), 2)
+
+    def save(self):
+        __name_file = "Ellipses.txt"
+        with open(__name_file, 'a', encoding='utf-8') as file:
+            file.writelines(["Ellipse,", str(self.__width_el) + ',', str(self.__height_el),
+                             str(self.__coordinate) + ',', str(self.perimeter()) + ',', str(self.area())])
+
+    def load(self):
+        ...
