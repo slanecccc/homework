19b044341b9eadbc8f7a6b40e60718b7bdcb1f7f
Goryachev K.S. <google.kost@yandex.ru>
2024-03-18 00:54:29 +0300

    Реализовал функцию, которая принимает объекты класса Shape и записывает их  в отдельный файл

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ def main():
     ellipse = Ellipse((6, 7), 4, 10)
     list_figure = [square, rectangle, circle, ellipse]
 
+    def save_figure(shapes: Shape):
+        shape.save()
+
+    for shape in list_figure:
+        save_figure(shape)
+
 
 if __name__ == '__main__':
     main()
